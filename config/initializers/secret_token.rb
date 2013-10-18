b61aceccb60e898e76de854bd4f598959fa14838
dcrelling <dcrelling@paypal.com>
2013-10-18 17:29:07 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GroominatiProto::Application.config.secret_key_base = '176091f259c20a71dbf727c76ffef127fdd54f8d4f6ea537cceb9a4ae0f079d45b6397aac2cd8561593ff664ca2dfe500c0a8ce9c6787469f0c55bc73762d1f8'
