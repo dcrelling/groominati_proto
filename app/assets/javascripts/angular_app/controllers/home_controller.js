ae1dd687d99e9792cb40eff0c420c25c266d34d7
dcrelling <dcrelling@paypal.com>
2014-01-23 16:23:49 -0500

    added a form to create a post also added some logic to the application controller to allow the js client to pass a CSRF token with each non GET request

--- app/assets/javascripts/angular_app/controllers/home_controller.js
+++ app/assets/javascripts/angular_app/controllers/home_controller.js
@@ -1,3 +1,8 @@
 groominatiApp.controller('HomeCtrl', ['$scope', 'Post', function($scope, Post){
     $scope.posts = Post.query();
+
+    $scope.newPost = function(){
+       //alert($scope.post.content);
+       Post.save({content: $scope.post.content});
+    };
 }]);
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :set_csrf_cookie_for_ng  # this is for setting the csrf cookie in the client
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  #for each non GET request we need to check that the X-XSRF-TOKEN token sent from the client matches the server token
+  #this will prevent cross site request forgery when using ajax request for non GET
+  protected
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
   end
 
 end
