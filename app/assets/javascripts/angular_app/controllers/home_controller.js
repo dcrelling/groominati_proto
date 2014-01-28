6c47774c1a6687029eb7048fc071c7b54e05d317
dcrelling <dcrelling@paypal.com>
2014-01-27 22:12:08 -0500

    added a function for success, error and progress. Did some minor refactoring

--- app/assets/javascripts/angular_app/controllers/home_controller.js
+++ app/assets/javascripts/angular_app/controllers/home_controller.js
@@ -1,44 +1,48 @@
-groominatiApp.controller('HomeCtrl', ['$scope', 'Post', '$upload', function($scope, Post, $upload){
+groominatiApp.controller('HomeCtrl', ['$scope', 'Post', '$upload', function ($scope, Post, $upload) {
     $scope.posts = Post.query();
 
-    $scope.newPost = function(){
-       //var newPost = Post.save({content: $scope.post.content, asset: "dang"});
-        var newPost = Post.save({content: $scope.post.content});
-       $scope.posts.push(newPost);
-    };
+    $scope.newPost = function () {
+        //var newPost = Post.save({content: $scope.post.content, asset: "dang"});
+        //var newPost = Post.save({content: $scope.post.content});
+        //$scope.posts.push(newPost);
+        //TODO: need to check if there is a file in scope, if not do try to upload a file just push the content
+        $scope.upload = $upload.upload({
+            url: '/posts', //url of the REST end point
+            method: 'POST',
+            // headers: {'headerKey': 'headerValue'}, withCredential: true,
+            data: {content: $scope.post.content},
 
-    $scope.onFileSelect = function($files) {
-        //$files: an array of files selected, each file has name, size, and type.
-        for (var i = 0; i < $files.length; i++) {
-            var asset = $files[i];
+            // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
+            /* set file formData name for 'Content-Desposition' header. Default: 'file' */
+            //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+            /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
+            /*
+             formDataAppender: function(formData, key, val){
+             },*/
 
-            $scope.upload = $upload.upload({
-                url: '/posts', //url of the REST end point
-                method: 'POST',
-                // headers: {'headerKey': 'headerValue'}, withCredential: true,
-                data: {content: $scope.post.content},
-
-                // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
-                /* set file formData name for 'Content-Desposition' header. Default: 'file' */
-                //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
-                /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
-                /*
-                formDataAppender: function(formData, key, val){
-                },*/
-
-                file: asset,
-                fileFormDataName: 'asset'
+            file: $scope.post.asset,
+            fileFormDataName: 'asset'
 
             //for progress and success to work I think we need to use the shim
-            }).progress(function(evt) {
-                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-                }).success(function(data, status, headers, config) {
-                    // file is uploaded successfully
-                    console.log(data);
-                });
-            //.error(...)
-            //.then(success, error, progress);
+            //TODO: need to use the shim to get the progress and success to work need this so that when done we push the new image to the post array
+        }).then(function (response) {
+                $scope.posts.push(response.data);
+            }, function (error){
+                //TODO: need some error handling here
+            }, function (evt) {
+                //$scope.progress[index] = parseInt(100.0 * evt.loaded / evt.total);
+                //TODO: need to handle progress update
+            });
+        //.error(...)
+        //.then(success, error, progress);
+    };
 
+    $scope.onFileSelect = function ($files) {
+        //$files: an array of files selected, each file has name, size, and type.
+        //TODO: we are uploading only one file so don't need this for loop
+        for (var i = 0; i < $files.length; i++) {
+            var asset = $files[i];
+            $scope.post.asset = asset;
         }
     };
 }]);
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -74,7 +74,7 @@ class PostsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    #params.require(:post).permit(:content, :user_id, :asset, :tag_list)  //was not able to figure out how to pass all the params in post, revisit this
+    #params.require(:post).permit(:content, :user_id, :asset, :tag_list)            //was not able to figure out how to pass all the params in post, revisit this
     params.permit(:content, :user_id, :asset, :tag_list)
   end
 
