4e1ebbde34cfbf11375806032ebbb1cc89a16a06
dcrelling <dcrelling@paypal.com>
2014-03-05 11:05:38 -0500

    adding tag_list to the serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :content, :asset
+  attributes :id, :user_id, :content, :asset, :tag_list
 
   def asset
     "#{object.asset.url(:medium)}"
