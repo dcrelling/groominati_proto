cd9b6f6e38c849004eb1ae91b1180796de3a7b7d
dcrelling <dcrelling@paypal.com>
2014-03-12 21:53:42 -0400

    changing the heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,12 @@ GroominatiProto::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #config action mailer to send email
-  config.action_mailer.default_url_options = {:host => 'http://evening-ravine-7085.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => 'evening-ravine-7085.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address => "127.0.0.1",
       :port    => 25,
-      :domain  => 'http://evening-ravine-7085.herokuapp.com'
+      :domain  => 'evening-ravine-7085.herokuapp.com'
   }
 
   #paperclip config to use AWS S3
