c89471adcb11cb23eb668b82eb2aaaa5742e953e
dcrelling <dcrelling@paypal.com>
2014-03-18 15:14:10 -0400

    added image slider

--- app/assets/javascripts/angular_app/directives/image_slider.js
+++ app/assets/javascripts/angular_app/directives/image_slider.js
@@ -1,8 +1,35 @@
-groominatiApp.directive('imageSlider', function(){
+groominatiApp.directive('imageSlider', function () {
 
     return{
         restrict: 'AE',
-        replace:   'true',
+        replace: 'true',
+        scope: {
+            posts: '='
+        },
+        link: function (scope, elem, attrs) {
+
+            scope.currentIndex = 0; // Initially the index is at the first post
+
+            scope.next = function () {
+                scope.currentIndex < scope.posts.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
+            };
+
+            scope.prev = function () {
+                scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.posts.length - 1;
+            };
+
+            scope.$watch('currentIndex', function () {
+                scope.images.forEach(function (image) {
+                    image.visible = false; // make every image invisible
+                });
+
+                scope.images[scope.currentIndex].visible = true; // make the current image visible
+            });
+
+
+        },
+        template: JST['angular_app/templates/image_slider']()
 
     };
+
 });
