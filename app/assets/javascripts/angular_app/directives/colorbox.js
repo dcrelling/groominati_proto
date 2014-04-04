db64e7c056b37a0c5453f18b35267b0b2fc24029
dcrelling <dcrelling@paypal.com>
2014-04-04 15:15:26 -0400

    got gallery in color box to work and now removed the custom image slider directive

--- app/assets/javascripts/angular_app/directives/colorbox.js
+++ app/assets/javascripts/angular_app/directives/colorbox.js
@@ -6,7 +6,8 @@ groominatiApp.directive('colorBox', ['$rootScope', function ($rootScope) {
     return{
         restrict: 'A',
         link: function (scope, elem, attrs) {
-           elem.colorbox(attrs.colorbox);
+           //elem.colorbox(attrs.colorbox);
+           angular.element('a.group1').colorbox({ opacity:0.5 , rel:'group1' });
         }
     };
 
