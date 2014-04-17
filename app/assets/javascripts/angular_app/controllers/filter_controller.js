bfa4ba70ab7bc5a35836a69779757f09455cee38
dcrelling <dcrelling@paypal.com>
2014-04-17 16:51:19 -0400

    added a filter and had to put the filter controller in the home index for isotope filtering to work

--- app/assets/javascripts/angular_app/controllers/filter_controller.js
+++ app/assets/javascripts/angular_app/controllers/filter_controller.js
@@ -1,3 +1,12 @@
 groominatiApp.controller('FilterCtrl', ['$scope', function ($scope) {
     $scope.isFilterCollapsed = true;
+
+    $scope.tagAdded = function(tag){
+
+    }
+
+    $scope.tagRemoved = function(tag){
+
+    }
+
 }]);
