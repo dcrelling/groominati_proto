07e29f756f278ba20d4667b29523d3f42ec7bb7e
dcrelling <dcrelling@paypal.com>
2014-07-15 18:08:07 -0400

    refactored and are now using a bootstrap modal with a controller to do the image slider

--- app/assets/javascripts/angular_app/controllers/home_controller.js
+++ app/assets/javascripts/angular_app/controllers/home_controller.js
@@ -2,31 +2,37 @@ groominatiApp.controller('HomeCtrl', ['$scope', 'Post', '$upload', '$modal', fun
     //$scope.posts = Post.query(); not using this now because we are pre-loading data
 
     /*this is the function that is called on PostCtrl initialize with ng-init
-    * we are pre-loading the data from the home#index.html.erb file via json_for(@posts)
-    */
-    $scope.modalShown = false;
-    $scope.clickIndex = 0;
+     * we are pre-loading the data from the home#index.html.erb file via json_for(@posts)
+     */
 
-    $scope.setPostsOnInit = function(data){
+    $scope.setPostsOnInit = function (data) {
         $scope.posts = data;
         $scope.postsIndex = 0;
     },
 
 
-    $scope.openNewPostModalForm = function(){
-        var modalInstance = $modal.open({
-            template: JST['angular_app/templates/new_post_modal_form'](),
-            controller: 'NewPostModalCtrl',
-            resolve: {posts: function(){
-                return $scope.posts;
-            }}
-        });
-    },
+        $scope.openNewPostModalForm = function () {
+            var modalInstance = $modal.open({
+                template: JST['angular_app/templates/new_post_modal_form'](),
+                controller: 'NewPostModalCtrl',
+                resolve: {posts: function () {
+                    return $scope.posts;
+                }}
+            });
+        },
 
-    $scope.setClickIndex = function(index){
-        $scope.clickIndex = index;
-        $scope.modalShown = !$scope.modalShown;
-    }
+        $scope.openImageSliderModalForm = function (index) {
+            var modalInstance = $modal.open({
+                template: JST['angular_app/templates/image_slider'](),
+                controller: 'ImageSliderModalCtrl',
+                resolve: {posts: function () {
+                    return $scope.posts;
+                },
+                    index: function () {
+                        return index;
+                    }}
+            });
+        }
 
 }]);
 
---
+++ app/assets/javascripts/angular_app/controllers/image_slider_modal_controller.js
@@ -0,0 +1,30 @@
+groominatiApp.controller ("ImageSliderModalCtrl", ['$scope', '$modalInstance', 'posts', 'index', function ($scope, $modalInstance, posts, index) {
+
+    $scope.posts = posts;
+    $scope.index = index;
+
+
+    $scope.next = function () {
+        $scope.index < $scope.posts.length - 1 ? $scope.index++ : $scope.index = 0;
+    };
+
+    $scope.prev = function () {
+        $scope.index > 0 ? $scope.index-- : $scope.index = $scope.posts.length - 1;
+    };
+
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+    $scope.$watch('index', function () {
+        $scope.posts.forEach(function (post) {
+            post.visible = false; // make every image invisible
+        });
+
+        $scope.posts[$scope.index].visible = true; // make the current image visible
+    })
+}]);
